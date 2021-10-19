f7af96ef3451227bd2ed47eb098d6c0b359d96fc
BuildTools <unconfigured@null.spigotmc.org>
2021-10-19 05:38:10 -0400

    added files

--- 15_session/app.py
+++ 15_session/app.py
@@ -4,19 +4,56 @@ SoftDev
 K15 -- Sessions Greetings -- Login and Cookies
 2021-10-18
 """
-from flask import Flask
-from flask import render_template
+
+from flask import Flask  # facilitate flask webserving
+from flask import render_template  # facilitate jinja templating
 from flask import request
-app = Flask(__name__)
+from flask.helpers import make_response  # facilitate form submission
 
-@app.route("/")
-def display_login():
-    return render_template( 'login.html' )
+#the conventional way:
+#from flask import Flask, render_template, request
 
-@app.route("/auth_ed")
-def display_logged_in():
-    return render_template('response.html', username = request.args['username'], method = request.method)
+app = Flask(__name__)  # create Flask object
 
-if __name__ == "__main__": #false if this file imported as module
+
+@app.route("/", methods=['GET', 'POST'])
+def disp_loginpage():
+    """
+    renders the login page
+    """
+    return render_template('login.html')
+    #brings up the login.html page
+    #askes for inputs of a text and to press a submit button
+
+#https://pythonbasics.org/flask-cookies/
+@app.route("/auth_ed", methods=['GET', 'POST'])
+def authenticate():
+    """
+    renders response template, placing in the username and method retrieved from the request
+    """
+    user = request.args.get('username')
+    password = request.args.get('password')
+
+    resp = make_response(render_template(
+        'response.html', username=request.args.get('username'), method=request.method))
+    resp.set_cookie('user',user)
+    resp.set_cookie('password',password)
+    print("This is the USERNAME AND PASSWORD")
+    #still prints None, alt
+    #print(user)
+    #print(password)
+
+    
+
+    return resp
+    #brings up the response.html page with username and the method request sent to the page
+    #uses responses from previous inputs/method
+
+
+if __name__ == "__main__":
+    """
+    false if this file imported as module
+    debugging enabled    
+    """
     app.debug = True
     app.run()
