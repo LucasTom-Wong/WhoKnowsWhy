6e1609457b0f95af6cc628a2eb1a6029e9154083
BuildTools <unconfigured@null.spigotmc.org>
2021-10-19 23:20:25 -0400

    Updated files

--- 15_session/app.py
+++ 15_session/app.py
@@ -1,59 +1,107 @@
 """
-Ishraq Mahid, Lucas Tom-Wong (LTW), Tomas Acuna
+The Onions - Ishraq Mahid, Lucas Tom-Wong (LTW), Tomas Acuna
 SoftDev
 K15 -- Sessions Greetings -- Login and Cookies
 2021-10-18
 """
 
-from flask import Flask  # facilitate flask webserving
-from flask import render_template  # facilitate jinja templating
-from flask import request
-from flask.helpers import make_response  # facilitate form submission
+"""
+LTW - changes 23:13 10/19/2021
+Added additional comments
+removed line 24, "debug = True", seemed redundant since below there was "app.debug = True"
+removed welcome.html and incoporated it into response.html
+ as well as changed code in app.py from welcome to response
+Created a Go_Back button for FailedLogin.html
+once we finish, we can comment out the if debug code as it is only needed if we are debugging and console messages are annoying
+login.html has not been altered
+"""
 
-#the conventional way:
-#from flask import Flask, render_template, request
+from flask import Flask, render_template, request
+from flask import session
+from config import KEY
+#imports modules used
 
 app = Flask(__name__)  # create Flask object
 
+app.secret_key = KEY #creates a key which is equal to theh key from a config file on your pc
+
+#debug = True redundant
 
 @app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
     """
-    renders the login page
+    renders the login page or home page, depending on user login status
     """
-    return render_template('login.html')
-    #brings up the login.html page
-    #askes for inputs of a text and to press a submit button
+    if debug:
+        print(request.method)
+    try:
+        #The only thing that sends a POST request, piping it into here, is the logout button
+        if request.method == "POST":
+            #Sets 'user' key to None to signal no user logged in
+            session['user'] = None
+            return render_template('login.html')
 
-#https://pythonbasics.org/flask-cookies/
+        #Everything else is a GET req essentially
+        elif request.method == "GET":
+            if debug:
+                print(session['user'])
+
+            #Checks to see if user exists -> logs them in if it does
+            if session['user']:
+                return render_template('response.html', username = session['user'], method = request.method)
+
+            #returns login template otherwise
+            return render_template('login.html')
+
+    except:
+        #in scenario of bad juju, simply has user try to login
+        return render_template('login.html')
+
+
 @app.route("/auth_ed", methods=['GET', 'POST'])
 def authenticate():
     """
-    renders response template, placing in the username and method retrieved from the request
+    renders auth flow template and responds to user login
     """
-    user = request.args.get('username')
-    password = request.args.get('password')
+    #try catch and responsive failedlogin template depending on failure type
+    try:
+        if debug:
+            print(request.method)
 
-    resp = make_response(render_template(
-        'response.html', username=request.args.get('username'), method=request.method))
-    resp.set_cookie('user',user)
-    resp.set_cookie('password',password)
-    print("This is the USERNAME AND PASSWORD")
+        #only req type for this page is GET (although maybe not accurate of what is going on?)
+        if request.method == "GET":
+
+            user = request.args.get('username')
+            password = request.args.get('password')
+
+            if debug:
+                print("Username and Password HERE:")
+                print(user)
+                print(password)
+
+            #Hardcoded user and password
+            if(user == "Mr.Mykolyk" and password == "251"):
+                session['user'] = user
+                #renders template using credentials
+                return render_template('response.html', username = session['user'], method = request.method)
+
+            return render_template("FailedLogin.html", error = "an incorrect password or username")
+    except:
+        return render_template("FailedLogin.html", error = "bad juju")
+
+    #print("This is the USERNAME AND PASSWORD")
     #still prints None, alt
     #print(user)
     #print(password)
 
-    
-
-    return resp
-    #brings up the response.html page with username and the method request sent to the page
+        #brings up the response.html page with username and the method request sent to the page
     #uses responses from previous inputs/method
 
 
 if __name__ == "__main__":
     """
     false if this file imported as module
-    debugging enabled    
+    debugging enabled
     """
     app.debug = True
     app.run()
