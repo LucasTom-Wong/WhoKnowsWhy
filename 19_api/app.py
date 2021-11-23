53493902fbbb813d4f9a4ae166b43d8d01da758a
LucasTom-Wong <lgtw3450@gmail.com>
2021-11-23 08:42:09 -0500

    did somethign

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,6 +1,30 @@
 """
-LTW, Lia
-SoftDev pd1
+Lucas (LTW) Tom-Wong, Julia (Lia) Nelson
+SoftDev
 K19 -- Restful API
-2021 - 10 - 23
+2021-10-23
 """
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+def display_Info(storage):
+    explanation = storage(explanation)
+    url = storage.value(url)
+    return render_template("main.html", explanation, url)
+
+@app.route("/")
+def main():
+    nasa = request()
+    library_of_info = json.loads(nasa)
+    display_Info(library_of_info)
+    print(library_of_info)
+
+if __name__ == "__main__":
+    """
+    false if this file imported as module
+    debugging enabled
+    """
+    app.debug = True
+    app.run()
