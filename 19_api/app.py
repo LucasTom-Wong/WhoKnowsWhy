3beed142ce41d23925efaf46b8006b1a465f23cf
BuildTools <unconfigured@null.spigotmc.org>
2021-11-27 23:49:55 -0500

    Updated 19_api

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,31 +1,30 @@
-"""
-Orange: Lucas (LTW) Tom-Wong, Julia (Lia) Nelson
-SoftDev
-K19 -- Restful API
-2021-10-23
-"""
-from flask import Flask, render_template, request
+# Orange: Lucas (LTW) Tom-Wong, Julia (Lia) Nelson
+# SoftDev
+# K19 -- Restful API
+# 2021-10-23
+
+from flask import Flask, render_template
 import json
-import urllib.request
+import urllib
 
 app = Flask(__name__)
 
 def display_Info(ssstorage):
-    explanation = ssstorage.data[explanation]
-    url = ssstorage.data[url]
+    explanation = ssstorage['explanation']
+    url = ssstorage['url']
     return render_template("main.html", explanation, url)
 
 @app.route("/")
 def main():
-    nasa = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=BbBUhlazreBfN6DQvmGnAp6TcuBzsfUQJISPj1hK")
+    url = "https://api.nasa.gov/planetary/apod?api_key="
+    url += str(open("key_nasa.txt", "r").read())
+    nasa = urllib.request.urlopen(url)
     library_of_info = json.loads(nasa.read())
     display_Info(library_of_info)
     # print(library_of_info)
 
 if __name__ == "__main__":
-    """
-    false if this file imported as module
-    debugging enabled
-    """
+    # false if this file imported as module
+    # debugging enabled
     app.debug = True
     app.run()
