e9243d50399e605218fcc18c410ab71b0fb3fc3c
LucasTom-Wong <lgtw3450@gmail.com>
2022-02-17 08:39:09 -0500

    movie logo

--- 32_js-anim/animate.js
+++ 32_js-anim/animate.js
@@ -1,28 +1,29 @@
-// Team Thumbs :: Oscar, Lucas
+// Team
 // SoftDev pd1
 // K31 -- canvas based JS animation
-// 2022-02-15
+// 2022-02-17
 
 // model for HTML5 canvas-based animation
-
-// SKEELTON
-
 
 //access canvas and buttons via DOM
 var c = document.getElementById("playground");// GET CANVAS
 var dotButton = document.getElementById("buttonCircle");// GET DOT BUTTON
 var stopButton = document.getElementById("buttonStop");// GET STOP BUTTON
+var movieButton = document.getElementById("buttonMovie");
 
 //prepare to interact with canvas in 2D
 var ctx = c.getContext("2d");// YOUR CODE HERE
-
 //set fill color to team color
 ctx.fillStyle = "#B4CEB3";// YOUR CODE HERE
 
 var requestID;  //init global var for use with animation frames
-// var start = Date.now();
+// let speed = 0; //number you see
 
-let speed = 0; //number you see
+var radius = 0;
+var radius_growth = 1; //i had it manually press to increase/decrease size because animation is hard, so it was greater than 1 at some point
+var growing = true;
+
+var image = 
 
 //var clear = function(e) {
 var clear = (e) => {
@@ -30,10 +31,6 @@ var clear = (e) => {
   ctx.clearRect(0, 0, c.width, c.height);
   // YOUR CODE HERE
 };
-
-var radius = 0;
-var radius_growth = 1; //i had it manually press to increase/decrease size because animation is hard, so it was greater than 1 at some point
-var growing = true;
 
 var drawCircle = function(){
   ctx.beginPath();
@@ -47,23 +44,25 @@ var drawDot = () => {
   window.cancelAnimationFrame(requestID);
   console.log("drawDot invoked...")
   // YOUR CODE HERE
+
+  if (radius >= (c.width / 2)){
+    growing = false;
+  }
+  if (radius <= 0){
+    growing = true;
+  }
+
   if (growing){
-    if (radius >= c.width) growing = false;
-    else {
-      console.log("growing");
-      clear();
-      radius+=radius_growth;
-      drawCircle();
-    }
+    console.log("growing");
+    clear();
+    radius+=radius_growth;
+    drawCircle();
   }
   else{
-    if (radius <= 0) growing = true;
-    else{
-      console.log("shrinking");
-      clear();
-      radius-=radius_growth;
-      drawCircle();
-    }
+    console.log("shrinking");
+    clear();
+    radius-=radius_growth;
+    drawCircle();
   }
   requestID = window.requestAnimationFrame(drawDot);
   /*
@@ -77,7 +76,7 @@ var drawDot = () => {
     window.requestAnimationFrame()
 
    */
-};
+}
 
 //var stopIt = function() {
 var stopIt = () => {
@@ -101,13 +100,22 @@ let display_Speed = function(){
   text.innerHTML = speed;
 }
 
+var movieTime = function(){
+
+}
+
 dotButton.addEventListener("click", function(){
   drawDot();
   // speed+=1;
   // display_Speed();
 });
 stopButton.addEventListener("click", function(){
-  stopIt();
   // speed-=1;
   // display_Speed();
+  stopIt();
 });
+
+movieButton.addEventListener("click", function(){
+  stopIt();
+  clear();
+})
